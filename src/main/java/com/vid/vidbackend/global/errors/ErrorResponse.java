3b5358d3a8156020448d5329d227bd849e58224f
yoon <leeyt1201@gmail.com>
2023-06-04 05:03:23 +0900

    [VB-14] feat: implemented error response object

--- src/main/java/com/vid/vidbackend/global/errors/ErrorResponse.java
+++ src/main/java/com/vid/vidbackend/global/errors/ErrorResponse.java
@@ -1,52 +1,69 @@
 package com.vid.vidbackend.global.errors;
 
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
 
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
 
-    private LocalDateTime timestamp;
-    private int code;
+    private String code;
     private String status;
     private String message;
-    private String stackTrace;
-    private Object data;
+    private List<FieldError> errors;
 
-    private ErrorResponse() {
-        timestamp = LocalDateTime.now();
+    private ErrorResponse(final ErrorCode code) {
+        this.code = code.getCode();
+        this.status = code.getStatus();
+        this.message = code.getMessage();
     }
 
-    private ErrorResponse(final HttpStatus httpStatus, final String message) {
-        this();
-        this.code = httpStatus.value();
-        this.status = httpStatus.name();
-        this.message = message;
+    private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+        this(code);
+        this.errors = errors;
     }
 
-    private ErrorResponse(final HttpStatus httpStatus, final String message, final String stackTrace) {
-        this(httpStatus, message);
-        this.stackTrace = stackTrace;
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
     }
 
-    private ErrorResponse(final HttpStatus httpStatus, final String message, final String stackTrace, final Object data) {
-        this(httpStatus, message, stackTrace);
-        this.data = data;
+    public static ErrorResponse of(final ErrorCode code, List<FieldError> errors) {
+        return new ErrorResponse(code, errors);
     }
 
-    public static ErrorResponse of(final HttpStatus httpStatus, final String message) {
-        return new ErrorResponse(httpStatus, message);
+    public static ErrorResponse of(final ErrorCode code, BindingResult bindingResult) {
+        return new ErrorResponse(code, FieldError.of(bindingResult));
     }
 
-    public static ErrorResponse of(final HttpStatus httpStatus, final String message, final String stackTrace) {
-        return new ErrorResponse(httpStatus, message, stackTrace);
-    }
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
 
-    public static ErrorResponse of(final HttpStatus httpStatus, final String message, final String stackTrace, final Object data) {
-        return new ErrorResponse(httpStatus, message, stackTrace, data);
+        private FieldError(final String field, final String value, final String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final  String value, final String reason) {
+            return List.of(new FieldError(field, value, reason));
+        }
+
+        public static List<FieldError> of(final BindingResult bindingResult) {
+            return bindingResult.getFieldErrors().stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
     }
 }
