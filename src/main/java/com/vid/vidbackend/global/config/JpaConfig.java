0b2476d6891b6f3d1d21b3e150763b8e59ed14e3
yoon <leeyt1201@gmail.com>
2023-06-05 01:25:35 +0900

    [VB-15] feat: implemented database replication(master/slave)

---
+++ src/main/java/com/vid/vidbackend/global/config/JpaConfig.java
@@ -0,0 +1,51 @@
+package com.vid.vidbackend.global.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class JpaConfig {
+
+    @Primary
+    @Bean("dataSource")
+    public DataSource dataSource(DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+
+    @Bean("routingDataSource")
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") DataSource masterDataSource,
+            @Qualifier("slaveDataSource") DataSource slaveDataSource) {
+        final Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put(RoutingDataSource.MASTER, masterDataSource);
+        dataSourceMap.put(RoutingDataSource.SLAVE, slaveDataSource);
+
+        final RoutingDataSource routingDataSource = new RoutingDataSource();
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(slaveDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean("masterDataSource")
+    @ConfigurationProperties(prefix = "database.master")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean("slaveDataSource")
+    @ConfigurationProperties(prefix = "database.slave")
+    public DataSource slaveDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
