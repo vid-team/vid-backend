47ddbc761fdd8b83dc7d21647a0aaff12c39c0d2
yoon <leeyt1201@gmail.com>
2023-06-04 10:29:06 +0900

    [VB-14] feat: added method not support error code

--- src/main/java/com/vid/vidbackend/global/error/model/ErrorCode.java
+++ src/main/java/com/vid/vidbackend/global/error/model/ErrorCode.java
@@ -2,14 +2,14 @@ package com.vid.vidbackend.global.error.model;
 
 import lombok.Getter;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 public enum ErrorCode {
     // Common
     SERVER_ERROR(INTERNAL_SERVER_ERROR.value(), "C001", "Server Error"),
     INVALID_INPUT_VALUE(UNPROCESSABLE_ENTITY.value(), "C002", "Invalid Input Value"),
+    METHOD_NOT_SUPPORT(METHOD_NOT_ALLOWED.value(), "G002", "Not Allowed HTTP Method"),
     ;
 
     private final int status;
