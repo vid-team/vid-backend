19229a9ffcd9428173a17ea5d1df95f1ccd42d75
yoon <leeyt1201@gmail.com>
2023-06-22 01:35:14 +0900

    [VB-18] style: changed using all arguments constructor

---
+++ src/main/java/com/vid/vidbackend/domain/notification/entity/Notification.java
@@ -0,0 +1,50 @@
+package com.vid.vidbackend.domain.notification.entity;
+
+import com.vid.vidbackend.domain.user.entity.User;
+import com.vid.vidbackend.global.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String message;
+
+    @Column(nullable = false)
+    private boolean isRead;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void read() {
+        if (this.isRead) {
+            return;
+        }
+        this.isRead = true;
+    }
+}
