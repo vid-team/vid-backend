e73447dd845fc987bd674253b6cea19aed227666
yoon <leeyt1201@gmail.com>
2023-06-21 21:05:39 +0900

    [VB-16] feat: implemented address entity

--- src/main/java/com/vid/vidbackend/domain/user/entity/Address.java
+++ src/main/java/com/vid/vidbackend/domain/user/entity/Address.java
@@ -1,17 +1,47 @@
 package com.vid.vidbackend.domain.user.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 
-@Embeddable
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Address {
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int zipcode;
+
+    @Column(nullable = false)
+    private String line;
+
+    @Column(nullable = false)
+    private String detail;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Address(Long id, String title, int zipcode, String line, String detail, String description, User user) {
+        this.id = id;
+        this.title = title;
+        this.zipcode = zipcode;
+        this.line = line;
+        this.detail = detail;
+        this.description = description;
+        this.user = user;
+    }
 }
