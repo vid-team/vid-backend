5e5022b319118bf24b20d2a8414ecf957882dd98
yoon <leeyt1201@gmail.com>
2023-06-21 22:32:28 +0900

    [VB-16] feat: implemented address entity

--- src/main/java/com/vid/vidbackend/domain/user/entity/Address.java
+++ src/main/java/com/vid/vidbackend/domain/user/entity/Address.java
@@ -5,10 +5,20 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Objects;
+
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Getter
 public class Address {
 
@@ -16,26 +26,26 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private int zipcode;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 200)
     private String line;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 200)
     private String detail;
 
+    @Column(length = 200)
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Builder
-    public Address(Long id, String title, int zipcode, String line, String detail, String description, User user) {
+    private Address(Long id, String title, int zipcode, String line, String detail, String description, User user) {
         this.id = id;
         this.title = title;
         this.zipcode = zipcode;
@@ -43,5 +53,18 @@ public class Address {
         this.detail = detail;
         this.description = description;
         this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
