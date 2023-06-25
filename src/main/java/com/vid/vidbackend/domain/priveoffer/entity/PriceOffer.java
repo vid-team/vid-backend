dffd2ab37a03096a060220db469d4d4a70583206
yoon <leeyt1201@gmail.com>
2023-06-25 23:53:46 +0900

    [VB-23] feat: implemented price offer entity

---
+++ src/main/java/com/vid/vidbackend/domain/priveoffer/entity/PriceOffer.java
@@ -0,0 +1,33 @@
+package com.vid.vidbackend.domain.priveoffer.entity;
+
+import com.vid.vidbackend.global.domain.MutableBaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+public class PriceOffer extends MutableBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private boolean isAccepted;
+
+}
