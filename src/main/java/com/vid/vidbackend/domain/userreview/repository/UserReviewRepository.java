ce481ab3d1943090110e6b8d2c221bbba70c9f15
yoon <leeyt1201@gmail.com>
2023-08-25 23:50:29 +0900

    [VB-14] feat: implemented user review jpa repository

---
+++ src/main/java/com/vid/vidbackend/domain/userreview/repository/UserReviewRepository.java
@@ -0,0 +1,6 @@
+package com.vid.vidbackend.domain.userreview.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReviewRepository extends JpaRepository<Long, UserReviewRepository> {
+}
